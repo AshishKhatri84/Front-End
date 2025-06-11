c924188c4633d8ceb56348c459bf393ff334d4e5
Ashish Khatri <44234ashish@gmail.com>
2025-06-11 15:32:24 +0530

    Delete Plan-It directory

--- Plan-It/errors/index.ts
+++
@@ -1,6 +0,0 @@
-import CustomAPIError from "./custom-error"
-import BadRequestError from "./bad-request"
-import UnauthenticatedError from "./unauthorized"
-import NotFoundError from "./not-found"
-
-export { CustomAPIError, BadRequestError, UnauthenticatedError, NotFoundError }
